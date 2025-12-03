ba4992c24561424971c19c9594fdaa51be6c0a28
mvegav <marina.vega@edu.xunta.es>
2025-12-03 12:01:42 +0100

    3-12-25

---
+++ UD7/steps en animaciones CSS/js/reloj.js
@@ -0,0 +1,52 @@
+// (function () {
+//   // calculateLines();
+//   setInterval(() => {
+//    calculateHourDegrees();
+//    calculateMinuteDegrees();
+//    calculateSeconds();
+//   }, 1);
+// })();
+const r =document.querySelector(':root');
+
+window.addEventListener('load',function () {
+   calculateHourDegrees();
+   calculateMinuteDegrees();
+    calculateSeconds();
+   });
+
+function linearMap(value, min, max, newMin, newMax) {
+    return newMin + (newMax - newMin) * (value - min) / (max - min);
+}
+
+function calculateHourDegrees() {
+  const currentHour = new Date().getHours() - 12;
+  const angle = linearMap(currentHour, 0, 12, 0, 360);
+  document.querySelector(".hours").style.transform = `rotate(${angle}deg)`;
+  // r.style.setProperty ("--rotacion", `${angle}deg`);
+  // console.log(getComputedStyle(r).getPropertyValue('--rotacion'));
+}
+
+function calculateMinuteDegrees() {
+  const currentMinutes = new Date().getMinutes();
+  const angle = linearMap(currentMinutes, 0, 60, 0, 360);
+  document.querySelector(".minutes").style.transform = `rotate(${angle}deg)`;
+  // r.style.setProperty ("--rotacion", `${angle}deg`);
+}
+
+function calculateSeconds() {
+  const currentSeconds = new Date().getSeconds();
+  const angle = linearMap(currentSeconds, 0, 60, 0, 360);
+  document.querySelector(".seconds").style.transform = `rotate(${angle}deg)`;
+  // r.style.setProperty ("--rotacion", `${angle}deg`);
+ }
+
+
+// function calculateLines() {
+//   const lines = document.querySelectorAll(".line");
+//   const numberLines = lines.length;
+//   for (let i = 0; i < numberLines; i++) {
+//     const line = lines[i];
+//     const angle = linearMap(i, 0, numberLines, 0, 360);
+//     line.style.transform = `rotate(${angle}deg)`;
+//   }
+// }
